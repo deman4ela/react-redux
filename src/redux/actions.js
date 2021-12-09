9d4c3f2185aaa40d0f4d5cc5d5ab0494b4876180
Dmytro Chyrkov <dchyr@softserveinc.com>
2021-12-09 18:21:38 +0200

    redux-actions implementation

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,16 +1,23 @@
-import { FETCH_PLACEHOLDER_DATA, FETCH_PLACEHOLDER_DATA_SUCCESS, FETCH_PLACEHOLDER_DATA_ERROR } from "./types";
+import { createAction } from 'redux-actions';
 
 
-export const fetchPlaceholderData = () => ({
-    type: FETCH_PLACEHOLDER_DATA,
-  });
+
+export const fetchPlaceholderData = createAction('FETCH_PLACEHOLDER_DATA')
+export const fetchPlaceholderDataSuccess = createAction('FETCH_PLACEHOLDER_DATA_SUCCESS')
+export const fetchPlaceholderDataError = createAction('FETCH_PLACEHOLDER_DATA_ERROR')
+
+
+
+// export const fetchPlaceholderData = () => ({
+//     type: FETCH_PLACEHOLDER_DATA,
+//   });
   
-  export const fetchPlaceholderDataSuccess = (data) => ({
-    type: FETCH_PLACEHOLDER_DATA_SUCCESS,
-    payload: data,
-  });
+//   export const fetchPlaceholderDataSuccess = (data) => ({
+//     type: FETCH_PLACEHOLDER_DATA_SUCCESS,
+//     payload: data,
+//   });
   
-  export const fetchPlaceholderDataError = (err) => ({
-    type: FETCH_PLACEHOLDER_DATA_ERROR,
-    payload: err,
-  });
+//   export const fetchPlaceholderDataError = (err) => ({
+//     type: FETCH_PLACEHOLDER_DATA_ERROR,
+//     payload: err,
+//   });
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { FETCH_PLACEHOLDER_DATA_SUCCESS, FETCH_PLACEHOLDER_DATA_ERROR } from "./types";
+import { handleActions } from "redux-actions"
 
 const defaultState = {
     visitorName: 'Dima',
@@ -6,18 +6,32 @@ const defaultState = {
     placeholderDataError: ''
 }
 
-const appReducer = (state = defaultState, action) => {
-    switch(action.type) {
+const appReducer = handleActions(
+    {
+        FETCH_PLACEHOLDER_DATA_SUCCESS: (state, action) => ({
+          ...state,
+          placeholderDataSuccess: action.payload,
+          placeholderDataError: ''
+        }),
+        FETCH_PLACEHOLDER_DATA_ERROR: (state, action) => ({
+          ...state,
+          placeholderDataError: action.payload
+      })},
+        defaultState
+  )
 
-        case FETCH_PLACEHOLDER_DATA_SUCCESS:
-            return { ...state, placeholderDataSuccess: action.payload,  placeholderDataError: ''};
+// const appReducer = (state = defaultState, action) => {
+//     switch(action.type) {
 
-        case FETCH_PLACEHOLDER_DATA_ERROR:
-            return { ...state, placeholderDataError: action.payload};
+//         case FETCH_PLACEHOLDER_DATA_SUCCESS:
+//             return { ...state, placeholderDataSuccess: action.payload,  placeholderDataError: ''};
 
-    default:
-      return state
-  }
-}
+//         case FETCH_PLACEHOLDER_DATA_ERROR:
+//             return { ...state, placeholderDataError: action.payload};
+
+//     default:
+//       return state
+//   }
+// }
 
 export default appReducer
