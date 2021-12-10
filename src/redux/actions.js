526bbd58d40f405ef88e08952f22432f453e8185
dimachyrkov <88532865+deman4ela@users.noreply.github.com>
2021-12-10 11:55:03 +0200

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,19 +5,3 @@ import { createAction } from 'redux-actions';
 export const fetchPlaceholderData = createAction('FETCH_PLACEHOLDER_DATA')
 export const fetchPlaceholderDataSuccess = createAction('FETCH_PLACEHOLDER_DATA_SUCCESS')
 export const fetchPlaceholderDataError = createAction('FETCH_PLACEHOLDER_DATA_ERROR')
-
-
-
-// export const fetchPlaceholderData = () => ({
-//     type: FETCH_PLACEHOLDER_DATA,
-//   });
-  
-//   export const fetchPlaceholderDataSuccess = (data) => ({
-//     type: FETCH_PLACEHOLDER_DATA_SUCCESS,
-//     payload: data,
-//   });
-  
-//   export const fetchPlaceholderDataError = (err) => ({
-//     type: FETCH_PLACEHOLDER_DATA_ERROR,
-//     payload: err,
-//   });
